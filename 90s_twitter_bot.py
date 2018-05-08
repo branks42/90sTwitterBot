00c1990e74a0278fd5d36e2b6dacb0257f11ca50
Justin Branco <justin.brancode@gmail.com>
2018-05-08 17:29:36 -0400

    Update 90s_twitter_bot.py

--- 90s_twitter_bot.py
+++ 90s_twitter_bot.py
@@ -5,10 +5,10 @@ import tweepy, time
 import random
 
 # Credentials
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_KEY = ''
-ACCESS_SECRET = ''
+CONSUMER_KEY = 'Your'
+CONSUMER_SECRET = 'Keys'
+ACCESS_KEY = 'Go'
+ACCESS_SECRET = 'Here'
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
